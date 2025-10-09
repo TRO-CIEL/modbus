4c054672007398af9dd1e1c15bb0d272d0b4360a
Téo Trompier <teo.trompier@gmail.com>
2025-10-09 11:19:12 +0200

    Commit #5

--- Form1.cs
+++ Form1.cs
@@ -1,199 +1,149 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
 
 namespace modbus
 {
-    public partial class Form1 : Form
+    public class CModbus
     {
-        // Socket - connexion
         private Socket socket;
-        
-        public Form1()
-        {
-            InitializeComponent();
-        }
 
-        // Événement - clic
-        private void buttonConnexion_Click(object sender, EventArgs e)
+        public string Connexion(string adresseIP)
         {
             try
             {
-                // Récupération - adresse
-                string adresseIP = textBoxAdresseIP.Text;
-                
-                // Affichage - tentative
-                textBoxStatut.Text += $"Connexion au serveur {adresseIP}\r\n";
-                
-                // Création - socket
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                
-                // Conversion - adresse
-                IPAddress ipAddress = IPAddress.Parse(adresseIP);
-                
-                // Création - endpoint
-                IPEndPoint endPoint = new IPEndPoint(ipAddress, 502); // Port Modbus standard
-                
-                // Connexion - serveur
-                socket.Connect(endPoint);
-                
-                // Affichage - succès
-                textBoxStatut.Text += "Connexion ok\r\n";
-                
-                // Défilement - automatique
-                textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                textBoxStatut.ScrollToCaret();
+                var ip = IPAddress.Parse(adresseIP);
+                var ep = new IPEndPoint(ip, 502);
+                socket.Connect(ep);
+                return "ok";
             }
-            catch (System.Net.Sockets.SocketException se)
+            catch (Exception ex)
             {
-                // Erreur - socket
-                textBoxStatut.Text += "**Exception : Impossible de se connecter au serveur\r\n";
-                textBoxStatut.Text += "Message : " + se.Message + "\r\n";
-                
-                // Défilement - automatique
-                textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                textBoxStatut.ScrollToCaret();
-            }
-            catch (System.Exception ex)
-            {
-                // Erreur - générale
-                textBoxStatut.Text += "**Exception : Impossible de se connecter au serveur\r\n";
-                textBoxStatut.Text += "Message : " + ex.Message + "\r\n";
-                
-                // Défilement - automatique
-                textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                textBoxStatut.ScrollToCaret();
+                return ex.Message;
             }
         }
 
-        // Événement - déconnexion
-        private void buttonDeconnexion_Click(object sender, EventArgs e)
+        public string Deconnexion()
         {
             try
             {
-                // Vérification - socket
                 if (socket != null)
                 {
-                    // Fermeture - connexion
                     socket.Close();
-                    
-                    // Affichage - succès
-                    textBoxStatut.Text += "Déconnexion réussie\r\n";
                 }
-                else
-                {
-                    // Affichage - aucune
-                    textBoxStatut.Text += "Aucune connexion active\r\n";
-                }
-                
-                // Défilement - automatique
-                textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                textBoxStatut.ScrollToCaret();
+                return "ok";
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
-                // Erreur - déconnexion
-                textBoxStatut.Text += "**Exception lors de la déconnexion\r\n";
-                textBoxStatut.Text += "Message : " + ex.Message + "\r\n";
-                
-                // Défilement - automatique
-                textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                textBoxStatut.ScrollToCaret();
+                return ex.Message;
             }
         }
 
-        // Événement - tension
-        private void buttonLire_Click(object sender, EventArgs e)
+        public short LireUnMot(short adresse, ref string strResultat)
         {
             try
             {
-                // Vérification - connexion
                 if (socket == null || !socket.Connected)
                 {
-                    textBoxStatut.Text += "Erreur : Pas de connexion active\r\n";
-                    textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
-                    textBoxStatut.ScrollToCaret();
-                    return;
+                    strResultat = "non connecté";
+                    return 0;
                 }
 
-                // Trame - lecture
-                var trameE = new byte[] { 
+                ushort start = (ushort)(adresse - 1);
+                var trameE = new byte[]
+                {
                     0x00, 0x00,
                     0x00, 0x00,
                     0x00, 0x06,
                     0x01,
                     0x03,
-                    0x0C, 0x86,
+                    (byte)(start >> 8), (byte)(start & 0xFF),
                     0x00, 0x01
                 };
 
-                // Affichage - envoi
-                textBoxStatut.Text += "Envoi demande lecture tension...\r\n";
+                socket.Send(trameE);
 
-                // Affichage - trame envoyée
-                string hexEnvoi = "Trame envoyée : ";
-                for (int i = 0; i < trameE.Length; i++)
-                {
-                    hexEnvoi += String.Format("{0:X2} ", trameE[i]);
-                }
-                textBoxStatut.Text += hexEnvoi + "\r\n";
-
-                // Envoi - trame
-                int bytesSent = socket.Send(trameE);
-                textBoxStatut.Text += $"Envoyé : {bytesSent} bytes\r\n";
-
-                // Réception - buffer
                 var trameR = new byte[256];
-                
-                // Réception - données
-                int bytesReceived = socket.Receive(trameR);
-                textBoxStatut.Text += $"Reçu : {bytesReceived} bytes\r\n";
-
-                // Affichage - hexadécimal
-                string hexString = "Trame reçue : ";
-                for (int i = 0; i < bytesReceived; i++)
+                int bytes = socket.Receive(trameR);
+                if (bytes >= 11 && trameR[7] == 0x03 && trameR[8] == 0x02)
                 {
-                    hexString += String.Format("{0:X2} ", trameR[i]);
+                    ushort val = (ushort)((trameR[9] << 8) | trameR[10]);
+                    strResultat = "ok";
+                    return unchecked((short)val);
                 }
-                textBoxStatut.Text += hexString + "\r\n";
+                strResultat = "réponse invalide";
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                strResultat = ex.Message;
+                return 0;
+            }
+        }
+    }
 
-                // Vérification - réponse
-                if (bytesReceived >= 9 && trameR[7] == 0x03)
+    public partial class Form1 : Form
+    {
+        private CModbus modbus;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            modbus = new CModbus();
+        }
+
+        private void buttonConnexion_Click(object sender, EventArgs e)
+        {
+            string adresseIP = textBoxAdresseIP.Text;
+            textBoxStatut.Text += $"Connexion au serveur {adresseIP}\r\n";
+            var res = modbus.Connexion(adresseIP);
+            if (res == "ok") textBoxStatut.Text += "Connexion ok\r\n"; else textBoxStatut.Text += "**Exception : Impossible de se connecter au serveur\r\nMessage : " + res + "\r\n";
+            textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
+            textBoxStatut.ScrollToCaret();
+        }
+
+        private void buttonDeconnexion_Click(object sender, EventArgs e)
+        {
+            var res = modbus.Deconnexion();
+            if (res == "ok") textBoxStatut.Text += "Déconnexion réussie\r\n"; else textBoxStatut.Text += "**Exception lors de la déconnexion\r\nMessage : " + res + "\r\n";
+            textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
+            textBoxStatut.ScrollToCaret();
+        }
+
+        private void buttonLire_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string res = "";
+                short val = modbus.LireUnMot(3207, ref res);
+                if (res == "ok")
                 {
-                    int tensionRaw = (trameR[9] << 8) | trameR[10];
-                    double tension = tensionRaw / 10.0;
-                    
-                    textBoxTension.Text = String.Format("{0:F1} V", tension);
-                    textBoxStatut.Text += String.Format("Tension : {0:F1} V (Raw: {1:X4})\r\n", tension, tensionRaw);
+                    double tension = ((ushort)val) / 10.0;
+                    textBoxTension.Text = string.Format("{0:F1} V", tension);
                 }
                 else
                 {
-                    // Affichage - erreur
-                    textBoxStatut.Text += "Erreur dans la réponse Modbus\r\n";
+                    textBoxTension.Text = "Erreur";
+                    textBoxStatut.Text += "**Exception lors de la lecture tension\r\nMessage : " + res + "\r\n";
                 }
-
-                // Défilement - automatique
                 textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
                 textBoxStatut.ScrollToCaret();
             }
             catch (System.Exception ex)
             {
                 textBoxTension.Text = "Erreur";
-                textBoxStatut.Text += "**Exception lors de la lecture tension\r\n";
-                textBoxStatut.Text += "Message : " + ex.Message + "\r\n";
-                
+                textBoxStatut.Text += "**Exception lors de la lecture tension\r\nMessage : " + ex.Message + "\r\n";
                 textBoxStatut.SelectionStart = textBoxStatut.Text.Length;
                 textBoxStatut.ScrollToCaret();
             }
         }
-
     }
 }
